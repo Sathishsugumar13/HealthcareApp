9243bb1d7f149ef5075882486a6190c206fe85b1
Sathish <sathishs@thestackly.com>
2026-09-21 10:04:28 +0530

    tab navigation

--- src/screens/Profile/ProfileScreen.tsx
+++ src/screens/Profile/ProfileScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, StyleSheet, SafeAreaView } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import ProfileContent from '../../components/Profile/ProfileContent';
 import { useProfileImage } from '../../hooks/useProfileImage';
 
